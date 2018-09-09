a06fcc0a7f4af3a6b7c79d0d518f8102ac330ff3
zixcon <zixcon.fan@outlook.com>
2018-09-09 20:29:00 +0800

    Update Grid.vue

---
+++ hello-ch1/src/components/base/Grid.vue
@@ -0,0 +1,8 @@
+<template>
+    <Table stripe :columns="columnsname" :data="columnsdata"></Table>
+</template>
+<script>
+    export default {
+        props: ['columnsname','columnsdata']
+    }
+</script>
