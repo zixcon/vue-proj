3f567cf033796863eb2f797951b55ee115e7ce6c
zixcon <zixcon.fan@outlook.com>
2018-09-09 20:29:27 +0800

    Update Page.vue

---
+++ hello-ch1/src/components/base/Page.vue
@@ -0,0 +1,22 @@
+<template>
+    <Page class="footer" :total="total" size="small" show-elevator show-sizer />
+</template>
+<script>
+    export default {
+        props: ['total']
+    }
+</script>
+<style>
+.footer{
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 50px;
+		text-align: center;
+		# background: #000000;
+		# color: #FFF000;
+		line-height: 50px;
+	}
+
+</style>
